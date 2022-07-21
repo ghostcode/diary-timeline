b2ae0986f6ea1dfc6a3e3db17e0c3e2f2a38b165
zhuxy <ghostcode521@gmail.com>
2022-07-21 13:04:00 +0800

    add navieui

---
+++ src/components/NavieUI.vue
@@ -0,0 +1,21 @@
+<script>
+  import {defineComponent} from 'vue'
+  import {NInput} from 'naive-ui'
+
+  export default defineComponent({
+    components:{
+      NInput,
+    },
+    setup(){
+      return {}
+    }
+  })
+</script>
+<template>
+  <div>
+    <NInput/>
+  </div>
+</template>
+<style>
+
+</style>
